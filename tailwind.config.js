dfc19a0e7920c7b76dfc8941dc71c3cbc26437ca
Jordan Welch <jordanhwelch@gmail.com>
2023-03-27 23:48:05 -0500

    Add wowell world

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wowpurp': '#8c52ff',
+        'lavendar-haze': '#d3c2f5',
+      },
+      fontFamily: {
+        'horizon': ['Horizon', 'sans-serif'],
+        'horizon-outlined': ['Horizon Outlined', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
