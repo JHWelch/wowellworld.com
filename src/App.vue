c3f88fb499a7ca5a20748ef439402a2d7a808ab8
Jordan Welch <jordanhwelch@gmail.com>
2024-10-05 21:20:49 -0500

    lint codebase

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,33 @@
 <template>
   <div class="bg-black font-horizon text-lavendar-haze">
     <div class="w-full bg-gradient-to-b from-black to-wowpurp min-h-screen">
-      <h1 class="sr-only">Wowell World</h1>
+      <h1 class="sr-only">
+        Wowell World
+      </h1>
 
-      <svg class="text-[33px] leading-[2.5rem]" viewBox="0 0 500 200">
-        <path id="curve" fill="transparent" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
-        <text stroke="currentColor" fill="currentColor" width="100%">
+      <svg
+        class="text-[33px] leading-[2.5rem]"
+        viewBox="0 0 500 200"
+      >
+        <path
+          id="curve"
+          fill="transparent"
+          d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
+        />
+        <text
+          stroke="currentColor"
+          fill="currentColor"
+          width="100%"
+        >
           <textPath xlink:href="#curve">
             Wowell World
           </textPath>
         </text>
       </svg>
 
-      <h2 class="text-xl sm:text-2xl md:text-3xl text-center lg:text-4xl">EST 2022</h2>
+      <h2 class="text-xl sm:text-2xl md:text-3xl text-center lg:text-4xl">
+        EST 2022
+      </h2>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{ts,js,vue}",
+    './index.html',
+    './src/**/*.{ts,js,vue}',
   ],
   theme: {
     extend: {
       colors: {
-        'wowpurp': '#8c52ff',
+        wowpurp: '#8c52ff',
         'lavendar-haze': '#d3c2f5',
       },
       fontFamily: {
-        'horizon': ['Horizon', 'sans-serif'],
+        horizon: ['Horizon', 'sans-serif'],
         'horizon-outlined': ['Horizon Outlined', 'sans-serif'],
       },
     },
