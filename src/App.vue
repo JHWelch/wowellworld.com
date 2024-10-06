27ddfd7e448be5fe1a232816f1bd49eea7496e5a
Jordan Welch <jordanhwelch@gmail.com>
2024-10-05 21:40:42 -0500

    add links

--- src/App.vue
+++ src/App.vue
@@ -3,34 +3,59 @@
 
 <template>
   <div class="bg-black font-horizon text-lavendar-haze">
-    <div class="w-full bg-gradient-to-b from-black to-wowpurp min-h-screen">
-      <h1 class="sr-only">
-        Wowell World
-      </h1>
+    <div class="w-full bg-gradient-to-b from-black to-wowpurp min-h-screen flex justify-center">
+      <div class="max-w-4xl w-full">
+        <h1 class="sr-only">
+          Wowell World
+        </h1>
 
-      <svg
-        class="text-[33px] leading-[2.5rem]"
-        viewBox="0 0 500 200"
-      >
-        <path
-          id="curve"
-          fill="transparent"
-          d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
-        />
-        <text
-          stroke="currentColor"
-          fill="currentColor"
-          width="100%"
-        >
-          <textPath xlink:href="#curve">
-            Wowell World
-          </textPath>
-        </text>
-      </svg>
+        <div>
+          <svg
+            class="text-[33px] leading-[2.5rem]"
+            viewBox="0 0 500 200"
+          >
+            <path
+              id="curve"
+              fill="transparent"
+              d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
+            />
+            <text
+              stroke="currentColor"
+              fill="currentColor"
+              width="100%"
+            >
+              <textPath xlink:href="#curve">
+                Wowell World
+              </textPath>
+            </text>
+          </svg>
+        </div>
 
-      <h2 class="text-xl sm:text-2xl md:text-3xl text-center lg:text-4xl">
-        EST 2022
-      </h2>
+        <div class="-mt-10 w-full flex flex-col items-center">
+          <h2 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            EST 2022
+          </h2>
+
+          <ul class="text-lg mt-10 space-y-6 text-center text-white">
+            <li>
+              <a
+                href="https://movies.wowellworld.com"
+                class="hover:underline"
+              >
+                Movies
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://games.wowellworld.com"
+                class="hover:underline"
+              >
+                Games
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
